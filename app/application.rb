6f2ca8c71e5f5585b77ad40179d0ef0327baf577
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-10-17 20:37:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,8 +5,9 @@ class Application
     request = Rack::Request.new(env)
 
     item_name = request.path.split("/items/").last
+    item = Items.item.find {|item| item.name == item_name}
     if request.path.match("/items/#{item_name}")
-      response.write Item.items
+      response.write item.price
     else
       response.write [404, {"Content-Type" => "text/html"}, ["Route not found"]]
     end
