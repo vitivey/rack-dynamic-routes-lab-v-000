bb5ed07a6a858986f62b85675fd5339c02994092
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-10-17 20:14:18 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,15 @@
 class Application
 
-  def call
+  def call(name)
     response = Rack::Response.new
     request = Rack::Request.new(env)
 
     if request.path.match(/items/"#{name}")
-
+      [404]
+    else
+      request.write @price
+    end
+    
   end
 
 end
