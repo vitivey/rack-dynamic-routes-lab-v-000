db32d3ab5b4a8da90d33ed3aadb35293e5f5033a
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-10-17 20:34:05 +0000

    automatically backed up by learn

--- app/item.rb
+++ app/item.rb
@@ -2,9 +2,10 @@ class Item
   attr_accessor :name, :price
 
   @@items =[]
+  
   def initialize(name,price)
     @name = name
     @price = price
-    @@item << self
+    @@items << self
   end
 end
