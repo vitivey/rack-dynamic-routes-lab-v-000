15152d469389a524a199d623b63e6713b6d64832
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-10-17 20:27:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     response = Rack::Response.new
     request = Rack::Request.new(env)
 
-    if request.path.match("/items/#{item.name}")
+    if request.path.match("/items/#{name}")
       response.write item.price
     else
 
